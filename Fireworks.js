1ba9863843b2e8f1696f02f8612b9070fc668567
P V Revanth <pvrevanth04@gmail.com>
2024-04-19 19:28:35 +0530

    Adding

--- Fireworks.js
+++ Fireworks.js
@@ -13,20 +13,17 @@ export class Fireworks {
         window.onresize = () => this.reset();
         this.reset();
     }
-
     createCanvas() {
         const c = document.createElement("canvas");
         document.body.appendChild(c);
         return c;
     }
-
     reset() {
         this.cwidth = window.innerWidth;
         this.cheight = window.innerHeight;
         this.canvas.width = this.cwidth;
         this.canvas.height = this.cheight;
     }
-
     newShell() {
         var left = (Math.random() > 0.5);
         var shell = {};
@@ -36,31 +33,23 @@ export class Fireworks {
         shell.yoff = 0.01 + Math.random() * 0.007;
         shell.size = Math.random() * 6 + 3;
         shell.color = this.colors[Math.floor(Math.random() * this.colors.length)];
-
         this.shells.push(shell);
     }
-
     newPass(shell) {
         var pasCount = Math.ceil(Math.pow(shell.size, 2) * Math.PI);
-
         for (let i = 0; i < pasCount; i++) {
             var pas = {};
             pas.x = shell.x * this.cwidth;
             pas.y = shell.y * this.cheight;
-
             var a = Math.random() * 4;
             var s = Math.random() * 10;
-
             pas.xoff = s * Math.sin((5 - a) * (Math.PI / 2));
             pas.yoff = s * Math.sin(a * (Math.PI / 2));
-
             pas.color = shell.color;
             pas.size = Math.sqrt(shell.size);
-
             if (this.pass.length < 1000) { this.pass.push(pas); }
         }
     }
-
     removeMission(missionName) {
         this.canvas.style.display = "block";
         this.gamebackgroundmusic.pause();
@@ -72,89 +61,62 @@ export class Fireworks {
             if (missions[i].innerText === missionName) {
                 missions[i].remove();
                 this.run();
-
                 var completedMissions = 5 - missionList.getElementsByTagName('li').length;
                 document.querySelector('#mission-button span').textContent = completedMissions;
-
-                this.points += 100; // Assuming each mission is worth 100 points
+                this.points += 100*(i+1); 
                 this.updatePoints();
                 break;
             }
         }
     }
-
     updatePoints() {
         var pointsElement = document.getElementById('points');
         pointsElement.innerHTML = `<img src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Rotating-golden-star.gif" alt="Star" />  ${this.points}`;
     }
-
     run() {
         var dt = 1;
         if (this.lastRun != 0) { dt = Math.min(50, (performance.now() - this.lastRun)); }
         this.lastRun = performance.now();
-
         this.ctx.fillStyle = "rgba(0,0,0,0.25)";
         this.ctx.fillRect(0, 0, this.cwidth, this.cheight);
-
         if ((this.shells.length < 10) && (Math.random() > 0.96)) { this.newShell(); }
-
         for (let ix in this.shells) {
             var shell = this.shells[ix];
-
             this.ctx.beginPath();
             this.ctx.arc(shell.x * this.cwidth, shell.y * this.cheight, shell.size, 0, 2 * Math.PI);
             this.ctx.fillStyle = shell.color;
             this.ctx.fill();
-
             shell.x -= shell.xoff;
             shell.y -= shell.yoff;
             shell.xoff -= (shell.xoff * dt * 0.001);
             shell.yoff -= ((shell.yoff + 0.2) * dt * 0.00005);
-
             if (shell.yoff < -0.005) {
                 this.newPass(shell);
                 this.shells.splice(ix, 1);
             }
         }
-
         for (let ix in this.pass) {
             var pas = this.pass[ix];
-
             this.ctx.beginPath();
             this.ctx.arc(pas.x, pas.y, pas.size, 0, 2 * Math.PI);
             this.ctx.fillStyle = pas.color;
             this.ctx.fill();
-
             pas.x -= pas.xoff;
             pas.y -= pas.yoff;
             pas.xoff -= (pas.xoff * dt * 0.001);
             pas.yoff -= ((pas.yoff + 5) * dt * 0.0005);
             pas.size -= (dt * 0.002 * Math.random());
-
             if ((pas.y > this.cheight) || (pas.y < -50) || (pas.size <= 0)) {
                 this.pass.splice(ix, 1);
             }
         }
-
         this.runAnimation = requestAnimationFrame(() => this.run());
-
         setTimeout(() => {
             cancelAnimationFrame(this.runAnimation);
             this.canvas.style.display = "none";
             document.getElementById('game').style.display = 'block';
             this.gamebackgroundmusic.play();
             this.fireworksbackgroundmusic.pause();
-        }, 5000); // Stop the animation after 5 seconds
+        }, 5000);
     }
 }
-
-// const fireworks = new Fireworks();
-
-// setTimeout(() => {
-//     fireworks.removeMission("MISSION_3");
-// }, 7000);
-
-// setTimeout(() => {
-//     fireworks.removeMission("MISSION_4");
-// }, 20000);
-
--- school.js
+++ school.js
@@ -1,41 +1,28 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
 import { LoadingBar } from './LoadingBar.js';
 import { SFX } from './libs/SFX.js'
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 class SpeechBubble {
     constructor(scene, msg, size = 1) {
         this.config = { font: 'Calibri', size: 14, padding: 10, colour: '#000', width: 256, height: 256 };
-
-        const planeGeometry = new THREE.PlaneGeometry(size, size);
-        const planeMaterial = new THREE.MeshBasicMaterial()
+        var planeGeometry = new THREE.PlaneGeometry(size, size);
+        var planeMaterial = new THREE.MeshBasicMaterial()
         this.mesh = new THREE.Mesh(planeGeometry, planeMaterial);
         scene.add(this.mesh);
-
-        const self = this;
-        const loader = new THREE.TextureLoader();
+        var self = this;
+        var loader = new THREE.TextureLoader();
         loader.load(
-            // resource URL
             `./assets/images/speech.png`,
-
-            // onLoad callback
             function (texture) {
-                // in this example we create the material when the texture is loaded
                 self.img = texture.image;
                 self.mesh.material.map = texture;
                 self.mesh.material.transparent = true;
                 self.mesh.material.needsUpdate = true;
                 if (msg !== undefined) self.update(msg);
             },
-
-            // onProgress callback currently not supported
             undefined,
-
-            // onError callback
             function (err) {
                 console.error('An error happened.');
             }
@@ -46,11 +33,9 @@ class SpeechBubble {
     }
     update(msg) {
         if (this.mesh === undefined) return;
-
         let context = this.context;
-
         if (this.mesh.userData.context === undefined) {
-            const canvas = this.createOffscreenCanvas(this.config.width, this.config.height);
+            var canvas = this.createOffscreenCanvas(this.config.width, this.config.height);
             this.context = canvas.getContext('2d');
             context = this.context;
             context.font = `${this.config.size}pt ${this.config.font}`;
@@ -58,33 +43,28 @@ class SpeechBubble {
             context.textAlign = 'center';
             this.mesh.material.map = new THREE.CanvasTexture(canvas);
         }
-
-        const bg = this.img;
+        var bg = this.img;
         context.clearRect(0, 0, this.config.width, this.config.height);
         context.drawImage(bg, 0, 0, bg.width, bg.height, 0, 0, this.config.width, this.config.height);
         this.wrapText(msg, context);
-
         this.mesh.material.map.needsUpdate = true;
     }
-
     createOffscreenCanvas(w, h) {
-        const canvas = document.createElement('canvas');
+        var canvas = document.createElement('canvas');
         canvas.width = w;
         canvas.height = h;
         return canvas;
     }
-
     wrapText(text, context) {
-        const words = text.split(' ');
+        var words = text.split(' ');
         let line = '';
-        const lines = [];
-        const maxWidth = this.config.width - 2 * this.config.padding;
-        const lineHeight = this.config.size + 8;
-
+        var lines = [];
+        var maxWidth = this.config.width - 2 * this.config.padding;
+        var lineHeight = this.config.size + 8;
         words.forEach(function (word) {
-            const testLine = `${line}${word} `;
-            const metrics = context.measureText(testLine);
-            const testWidth = metrics.width;
+            var testLine = `${line}${word} `;
+            var metrics = context.measureText(testLine);
+            var testWidth = metrics.width;
             if (testWidth > maxWidth) {
                 lines.push(line);
                 line = `${word} `;
@@ -92,17 +72,13 @@ class SpeechBubble {
                 line = testLine;
             }
         });
-
         if (line != '') lines.push(line);
-
         let y = (this.config.height - lines.length * lineHeight) / 2;
-
         lines.forEach(function (line) {
             context.fillText(line, 128, y);
             y += lineHeight;
         });
     }
-
     show(pos) {
         if (this.mesh !== undefined && this.player !== undefined) {
             this.mesh.position.set(this.player.object.position.x, this.player.object.position.y + 380, this.player.object.position.z);
@@ -110,98 +86,50 @@ class SpeechBubble {
         }
     }
 }
-const backgroundmusic=document.getElementById("bgAudio");
+var backgroundmusic=document.getElementById("bgAudio");
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 100);
 camera.position.set(3, 1.5, 1.5);
-camera.near = .015; // Set a smaller value, like 0.1
+camera.near = .015; 
 camera.updateProjectionMatrix();
-// Keyboard controls
-var moveForward = false;
-var moveBackward = false;
-var moveLeft = false;
-var moveRight = false;
-// Setup Gun Object
-var tommyGun;
 var loadingBar = new LoadingBar();
-// 3D Abandoned Building MOdel
 var abandonedBuilding;
-// Doctor1
-var doc1;
-// Doctor2
-var doc2;
 var colliders = [];
-
-// Create the renderer
 var renderer = new THREE.WebGLRenderer({ alpha: true, depth: true });
-// Configure renderer settings
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.toneMapping = THREE.ReinhardToneMapping;
-renderer.setClearColor(0x000000, 0); // Set background color to black
+renderer.setClearColor(0x000000, 0); 
 renderer.domElement.style.position = 'fixed';
 renderer.domElement.id = 'school';
 renderer.domElement.style.zIndex = '-1';
 renderer.domElement.style.left = '0';
 renderer.domElement.style.top = '0';
 document.body.appendChild(renderer.domElement);
-//Create ray caster instance
 var raycaster = new THREE.Raycaster();
-//Create mouse instance
 var mouse = new THREE.Vector2();
-///flashing light // Create a point light
-const tommyGunLight = new THREE.PointLight(0xffffff, 100, 100); // Adjust the light color and intensity as needed
-tommyGunLight.position.set(0, 0, 0); // Set the light position
+var tommyGunLight = new THREE.PointLight(0xffffff, 100, 100); 
+tommyGunLight.position.set(0, 0, 0); 
 tommyGunLight.visible = false
-// Add the light to the scene initially
 scene.add(tommyGunLight);
-// Add PointerLockControls
 var controls = new OrbitControls(camera, renderer.domElement);
-controls.mouseSensitivity = 0.005; // Adjust the sensitivity as needed
-controls.minDistance = 0; // Minimum zoom distance
-controls.maxDistance = 7; // Maximum zoom distance
+controls.mouseSensitivity = 0.005; 
+controls.minDistance = 0; 
+controls.maxDistance = 7; 
 controls.minPolarAngle = Math.PI / 4;
 controls.maxPolarAngle = Math.PI / 3;
-controls.maxAzimuthAngle = Math.PI / 2; // Maximum horizontal rotation (radians)
-controls.minAzimuthAngle = Math.PI / 7; // Minimum horizontal rotation (radians)
-
-// Create a grid
+controls.maxAzimuthAngle = Math.PI / 2; 
+controls.minAzimuthAngle = Math.PI / 7; 
 var gridHelper = new THREE.GridHelper(20, 20);
 scene.add(gridHelper);
-// Set up pointer lock controls
-// var blocker = document.getElementById('blocker');
-// var instructions = document.getElementById('instructions');
-// var playButton = document.getElementById('playButton');
-// playButton.addEventListener('click', function () {
-//     // controls.lock();
-//     instructions.style.display = 'none';
-//     blocker.style.display = 'none';
-// });
-// controls.addEventListener('lock', function () {
-//     instructions.style.display = 'none';
-//     blocker.style.display = 'none';
-// });
-// controls.addEventListener('unlock', function () {
-//     blocker.style.display = 'block';
-//     instructions.style.display = '';
-// });
-// Resize renderer when window size changes
 window.addEventListener('resize', function () {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
-// scene.add(controls.getObject());
-
-// Create an ambient light with brightness
-var ambientLight = new THREE.AmbientLight(0xffffff, 2); // Adjust the color as needed
+var ambientLight = new THREE.AmbientLight(0xffffff, 2); 
 scene.add(ambientLight);
-
-// Load GLTF model
 var loader = new GLTFLoader();
-
-
-// Load GLTF model
 loader.load(
     './assets/plane/school.glb',
     function (gltf) {
@@ -213,15 +141,11 @@ loader.load(
                 colliders.push(child);
                 child.castShadow = true;
                 child.receiveShadow = true;
-
             }
         });
         loadSFX();
         loadingBar.visible = false;
-        // After the model has loaded, update the message in the playButton div
-        // playButton.innerText = "Click To Explore";
     },
-    // Add a function to handle loading progress or errors (optional)
     function (xhr) {
         loadingBar.progress = (xhr.loaded/xhr.total);
     },
@@ -229,58 +153,13 @@ loader.load(
         console.error('An error occurred:', error);
     }
 );
-// const dracoLoader = new DRACOLoader();
-// loader.setDRACOLoader( dracoLoader );
-var doc1mixer;
 var hidemsgteacher = true;
-
-var teachervisited = false;
-
-
 var speechBubble_teacher;
-
-var escape=false;
 var sfx;
 function loadSFX() {
     sfx = new SFX(camera, './assets/sound/');
     sfx.load('teacher');
 }
-// loader.load(
-//     './assets/plane/doc1.glb',
-//     function (gltf) {
-//         doc1 = gltf.scene;
-//         doc1.position.set(1, 1.5, -1)
-//         doc1.scale.set(0.5, 0.5, 0.5)
-//         scene.add(doc1);
-//         doc1mixer = new THREE.AnimationMixer(gltf.scene);
-//         var idleanimation = gltf.animations[0];
-//         console.log(gltf.animations[0])
-//         var doc1action = doc1mixer.clipAction(idleanimation);
-//         doc1action.time = 0;
-//         doc1mixer.stopAllAction();
-//         doc1action.play();
-//         gltf.scene.traverse(function (child) {
-//             if (child.isMesh) {
-//                 colliders.push(child);
-//                 child.castShadow = true;
-//                 child.receiveShadow = true;
-
-//             }
-//         });
-
-
-//     },
-//     // Add a function to handle loading progress or errors (optional)
-//     function (xhr) {
-//         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-//     },
-//     function (error) {
-//         console.error('An error occurred:', error);
-//     }
-// );
-
-
-// Handle Mouse Click Event
 document.addEventListener('mousedown', onMouseDown, false);
 document.addEventListener('touchstart', onMouseDown, false);
 function addEscape() {
@@ -289,60 +168,50 @@ function addEscape() {
     element.style.width = '0px';
     element.style.height = '0px';
     document.body.appendChild(element);
+    document.removeEventListener('mousedown', onMouseDown, false);
+    document.removeEventListener('touchstart', onMouseDown, false);
 }
 function removeEscape(){
     var elm=document.getElementById('escape');
     if(elm){
         elm.remove();
+        document.addEventListener('mousedown', onMouseDown, false);
+        document.addEventListener('touchstart', onMouseDown, false);
     }
 }
 function onMouseDown(event) {
-    // Calculate normalized mouse coordinates
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-    // Update the picking ray from the camera
     raycaster.setFromCamera(mouse, camera);
-
-    // Check for intersections
-    const intersects = raycaster.intersectObjects(scene.children, true);
-
+    var intersects = raycaster.intersectObjects(scene.children, true);
     if (intersects.length > 0) {
-        // You've clicked on a 3D object!
         console.log('Clicked on a 3D model:', intersects[0].object.name);
         var name_obj = intersects[0].object.name;
         if (name_obj=="Ch12") {
-
             if (hidemsgteacher) {
                 backgroundmusic.pause();
                 speechBubble_teacher = new SpeechBubble(scene, "Why are you roaming here and there? Your performance in exams is degrading Better go home and complete your assignment.(Hint:Home--> Red -coloured closest to temple)", 1);
                 speechBubble_teacher.mesh.position.set(-2.6, 2, 3);
                 speechBubble_teacher.mesh.rotation.set(0, 4*Math.PI/8, 0);
                 sfx.play('teacher');
-                teachervisited=false;
                 removeEscape();
                 setTimeout(function() {
                     backgroundmusic.play();
                     speechBubble_teacher.hide();
-                    teachervisited = true;
-                        addEscape();
+                    addEscape();
                 }, 8000);
             }
         }
     }
 }
-const fbxloader = new FBXLoader();
+var fbxloader = new FBXLoader();
 let player = {};
 let animations = {};
 fbxloader.load('./assets/plane/Teacher.fbx', function (object) {
-
     object.mixer = new THREE.AnimationMixer(object);
-    // //console.log(object.mixer);
     player.mixer = object.mixer;
     player.root = object.mixer.getRoot();
-
     object.name = "FireFighter";
-
     object.traverse(function (child) {
         if (child.isMesh) {
             child.castShadow = true;
@@ -354,19 +223,8 @@ fbxloader.load('./assets/plane/Teacher.fbx', function (object) {
             colliders.push(child);
             child.castShadow = true;
             child.receiveShadow = true;
-
         }
     });
-
-    // const tLoader = new THREE.TextureLoader();
-    // tLoader.load(`${game.assetsPath}images/SimplePeople_FireFighter_Brown.png`, function(texture){
-    // 	object.traverse( function ( child ) {
-    // 		if ( child.isMesh ){
-    // 			child.material.map = texture;
-    // 		}
-    // 	} );
-    // });
-
     player.object = new THREE.Object3D();
     scene.add(player.object);
     player.object.add(object);
@@ -378,23 +236,20 @@ fbxloader.load('./assets/plane/Teacher.fbx', function (object) {
     player.object.rotation.set(0, Math.PI / 2, 0);  
     player.object.scale.set(0.009, 0.009, 0.009);
     set_action('Idle')
-    //game.loadNextAnim(loader);
-    // game.animate();
 });
 function set_action(name) {
-    const action = player.mixer.clipAction(animations[name]);
+    var action = player.mixer.clipAction(animations[name]);
     action.time = 0;
     player.mixer.stopAllAction();
     player.action = name;
     player.actionTime = Date.now();
     player.actionName = name;
-    //console.log("hel")
-    //action.fadeIn(0.5);	
     action.play();
 }
 var character = "boy";
-let char_player = {};
-let char_animations = {};
+if(document.getElementById("girl")){
+    character = "girl";
+}
 let path = `./assets/fbx/people/`
 if (character == "boy") {
     path += "school_boy/boy.fbx"
@@ -403,54 +258,30 @@ else if (character == "girl") {
     path += "school_girl/girl.fbx"
 }
 fbxloader.load(path, function (object) {
-
     object.mixer = new THREE.AnimationMixer(object);
-    // //console.log(object.mixer);
     player.mixer = object.mixer;
     player.root = object.mixer.getRoot();
-
-
-
     object.traverse(function (child) {
         if (child.isMesh) {
             child.castShadow = true;
             child.receiveShadow = false;
         }
     });
-
-    // const tLoader = new THREE.TextureLoader();
-    // tLoader.load(`${game.assetsPath}images/SimplePeople_FireFighter_Brown.png`, function(texture){
-    // 	object.traverse( function ( child ) {
-    // 		if ( child.isMesh ){
-    // 			child.material.map = texture;
-    // 		}
-    // 	} );
-    // });
-
     player.object = new THREE.Object3D();
     scene.add(player.object);
     player.object.add(object);
     animations.Idle = object.animations[0];
-
     console.log(object.animations[0]);
-
     player.object.position.set(.75, 0.4, 1.5);
     player.object.rotation.set(0, -2 * Math.PI / 4, 0);
     player.object.scale.set(0.007, 0.007, 0.007);
     set_action('Idle')
-    //loadNextAnim(fbxloader);
-    // game.animate();
 });
-
-
 let clock = new THREE.Clock();
 function animate() {
     requestAnimationFrame(animate);
-    const dt = clock.getDelta();
+    var dt = clock.getDelta();
     if (player.mixer !== undefined) player.mixer.update(dt);
-    if (doc1mixer !== undefined) doc1mixer.update(dt);
-    //ramp up player movement speed and direction
-
     renderer.render(scene, camera);
 }
 animate();
